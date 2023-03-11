3186c235d5a804755c0c34616534d40a5a11d26b
Ali Arthur Abel <arthur.ali@webitfactory.io>
2023-03-11 21:14:05 +0200

    push

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -81,7 +81,7 @@ export default function Input({
         <input
           {...props}
           ref={ref}
-          className='block w-full rounded-md border-0 pb-2 pt-6 pl-4 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          className='block w-full rounded-md border-0 pb-2 pt-6 pl-4 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
           placeholder=''
           defaultValue={undefined}
           value={value}
