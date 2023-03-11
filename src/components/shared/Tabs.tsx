440d9a8747cb30f944e4e9a8c01d1031582e1e66
Ivanov Cristina <ivanov.cristina99@gmail.com>
2023-03-11 14:37:26 +0200

    tabs

--- src/components/shared/Tabs.tsx
+++ src/components/shared/Tabs.tsx
@@ -8,13 +8,15 @@ interface TabsI {
 export default function Tabs({ tabs, activeTab, setActiveTab, style }: TabsI) {
   return (
     <div className="flex">
-      {tabs.map((tab: string, index: number) => {
+      {tabs.map((tab: string, index: any) => {
         return (
           <div
             onClick={() => setActiveTab(index)}
             className={
-              'rounded-md bg-[#f9f9f9] transition-all hover:bg-gray-200 px-3 py-1 mr-2 ' +
-              style
+              'my-2 mr-2 block min-w-[120px] rounded  px-6 pt-4 pb-3.5 text-center text-xs font-medium uppercase leading-tight md:mr-4 select-none cursor-pointer transition-all ' +
+              (activeTab === index
+                ? 'bg-blue-100 text-blue-700'
+                : 'bg-neutral-100 text-neutral-500 ')
             }
           >
             {tab}
