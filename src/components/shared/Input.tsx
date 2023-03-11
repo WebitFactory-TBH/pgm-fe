aa2f894e71528f0c5acabf83d1a1c93c0ecf1a1b
Ali Arthur Abel <arthur.ali@webitfactory.io>
2023-03-11 13:49:38 +0200

    Input component

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,40 +1,95 @@
-import { InputHTMLAttributes, useCallback, useRef, useState } from 'react';
+import {
+  ChangeEvent,
+  FocusEvent,
+  InputHTMLAttributes,
+  useCallback,
+  useRef,
+  useState
+} from 'react';
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  initValue?: string;
+  regexContrains?: string;
+  regexError?: string;
+}
 
-export default function Input({ ...props }: Props) {
+export default function Input({
+  initValue = '',
+  regexContrains = '[sS]*',
+  regexError,
+  ...props
+}: Props) {
   const ref = useRef<HTMLInputElement>(null);
   const [focus, setFocus] = useState(ref.current === document.activeElement);
+  const [value, setValue] = useState(initValue);
+  const [error, setError] = useState('test err');
 
-  const onFocus = useCallback(() => {
+  const onFocus = useCallback((event: FocusEvent<HTMLInputElement>) => {
     setFocus(true);
+    if (props.onFocus) {
+      props.onFocus(event);
+    }
   }, []);
 
-  const onBlur = useCallback(() => {
+  const onBlur = useCallback((event: FocusEvent<HTMLInputElement>) => {
     setFocus(false);
+    if (props.onBlur) {
+      props.onBlur(event);
+    }
+  }, []);
+
+  const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    const regex = new RegExp(regexContrains);
+
+    if (regex.test(value)) {
+      setValue(event.target.value);
+      return;
+    }
+
+    setError(regexError ?? 'Invalid value');
+    if (props.onChange) {
+      props.onChange(event);
+    }
   }, []);
 
   return (
-    <div className='relative'>
-      <label
-        htmlFor={props.name}
+    <div>
+      <div className='relative'>
+        <label
+          htmlFor={props.name}
+          className={`
+            absolute block text-sm font-bold leading-6 left-4 -translate-y-1/2
+            transition-all duration-300
+            ${
+              focus || value.length > 0
+                ? '-top-3 text-gray-900'
+                : 'top-1/2  text-gray-300'
+            }
+            `}
+        >
+          {props.placeholder}
+        </label>
+        <input
+          {...props}
+          ref={ref}
+          className='block w-full rounded-md border-0 py-3 pl-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          placeholder=''
+          value={value}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onChange={onChange}
+        />
+      </div>
+      <span
         className={`
-          absolute block text-sm font-bold leading-6 left-4 -translate-y-1/2
-          transition-all duration-300
-          ${focus ? '-top-3 text-gray-900' : 'top-1/2  text-gray-300'}
-          `}
+      animate-pulse text-red-500 font-thin text-xs 
+      transition-all duration-300
+      ${error.length > 0 ? 'opacity-1' : 'opacity-0'}
+      `}
       >
-        {props.placeholder}
-      </label>
-      <input
-        {...props}
-        ref={ref}
-        className='block w-full rounded-md border-0 py-3 pl-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-        placeholder=''
-        onFocus={onFocus}
-        onBlur={onBlur}
-        value={ref.current?.value}
-      />
+        {error}
+      </span>
     </div>
   );
 }
